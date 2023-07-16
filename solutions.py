36705b1bc09fb771bbf6e10d4ca5740dbc0f366b
Obukofe Joseph <119716469+JosephObukofe@users.noreply.github.com>
2023-07-16 06:27:53 +0100

    Update to solutions.py

--- solutions.py
+++ solutions.py
@@ -22,3 +22,80 @@ if __name__ == '__main__':
     print(a + b)
     print(a - b)
     print(a * b)
+
+# (4) - Python: Division
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
+
+# (5) - Loops
+if __name__ == '__main__':
+    n = int(input())
+    l = []
+    
+    while n > 0:
+        n = n - 1
+        l.append(n)
+        
+    l.reverse()
+    
+    for i in l:
+        print(i ** 2)
+
+# (6) - Write a function
+def is_leap(year):
+    leap = False
+    
+    if year % 4 != 0:
+        leap = False
+    elif year % 100 == 0 and year % 400 != 0:
+        leap = False
+    else:
+        leap = True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+# (7) - Print function
+if __name__ == '__main__':
+    n = int(input())
+    l = []
+    l.append(n)
+    while n > 1:
+        n = n - 1
+        l.append(n)
+    
+    l.reverse()
+    
+    for i in l:
+        print(str(i), end = '')
+
+# (8) - Arrays
+import numpy
+
+def arrays(arr):
+    return numpy.array(arr, dtype = float)[::-1]
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
+
+# (9) - Shape and Reshape
+import numpy as np
+
+def array_fun(arr):
+    arr = np.array(arr)
+    arr.shape = (3, 3)
+    return arr
+
+arr = input().strip().split(' ')
+list = []
+for i in arr:
+    i = int(i)
+    list.append(i)
+result = array_fun(list)
+print(result)
